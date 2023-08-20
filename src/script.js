f56749d8f4371033e5a43b0f51af74b708a553dc
andaluk <andaluk@gmail.com>
2023-08-20 10:27:49 +0300

    Финальный "правильный" код

--- src/script.js
+++ src/script.js
@@ -1,17 +1,27 @@
-function convertTemperature(value, unit) {
-  if (unit === "Celsius") {
-    return (value - 32) * 5 / 9;
-  } else if (unit === "Fahrenheit") {
-    return value * 9 / 5 + 32;
-  }
-}
+// Изолируем от глобального контекста
+(() => {
+  const fixed = 5;
 
-document
-  .getElementById("temperature-input")
-  .addEventListener("change", function () {
-    const temperature = parseFloat(this.value);
-    const unit = document.getElementById("unit-select").value;
-    const convertedTemperature = convertTemperature(temperature, unit);
-    document.getElementById("result").textContent =
-      convertedTemperature.toFixed(5);
-  });
+  // Сразу находим все элементы
+  const temperature = document.getElementById("temperature-input");
+  const selector = document.getElementById("unit-select");
+  const res = document.getElementById("result");
+
+  // Процедура конвертации
+  const convert = () => {
+    const value = parseFloat(temperature.value);
+    const r =
+
+      // Вычисляем в зависимости от значения селектора едениц измерения
+      selector.value === "Celsius"
+        ? ((value - 32) * 5) / 9
+        : (value * 9) / 5 + 32;
+
+    // Если получилось число выводим его, иначе сообщение о ошибке
+    res.textContent = isNaN(r) ? "Ошибка перевода" : r.toFixed(fixed);
+  };
+  // Заполняем листнеры событий нашей функцией
+  temperature.addEventListener("keyup", convert);
+  temperature.addEventListener("change", convert);
+  selector.addEventListener("change", convert);
+})();
