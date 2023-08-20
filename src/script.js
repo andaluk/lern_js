3cc82cecee5f09c650bef398c44e94636bfc5b4b
andaluk <andaluk@gmail.com>
2023-08-20 10:25:18 +0300

    Отладил, работает

--- src/script.js
+++ src/script.js
@@ -1,16 +1,17 @@
 function convertTemperature(value, unit) {
   if (unit === "Celsius") {
-    return ((value - 32) * 5) / 9;
+    return (value - 32) * 5 / 9;
   } else if (unit === "Fahrenheit") {
-    return (value * 9) / 5 + 32;
+    return value * 9 / 5 + 32;
   }
 }
+
 document
   .getElementById("temperature-input")
-  .addEvntListener("change", function () {
+  .addEventListener("change", function () {
     const temperature = parseFloat(this.value);
     const unit = document.getElementById("unit-select").value;
     const convertedTemperature = convertTemperature(temperature, unit);
-    document.getElementById("result").texContent =
-      convertedTemperature.ToFixed(5);
+    document.getElementById("result").textContent =
+      convertedTemperature.toFixed(5);
   });
