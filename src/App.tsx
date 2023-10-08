5d96a4e50f6e2b2a0fc1c1975fa0fd56d61eb5ce
andaluk <andaluk@gmail.com>
2023-10-08 15:44:37 +0300

    Создал форму, настроил валидацию.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,76 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  });
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        Имя:
+        <input
+          className={errors.username ? 'invalid' : ''}
+          type='text'
+          placeholder='Имя пользователя'
+          {...register('username', {
+            required: {
+              value: true,
+              message: 'Поле обязательно.',
+            },
+            minLength: {
+              value: 5,
+              message: 'Не менее 5 символов',
+            },
+            maxLength: {
+              value: 20,
+              message: 'Не более 20 символов',
+            },
+            pattern: {
+              value: /^[a-zA-Z0-9]+$/,
+              message: 'Только английские буквы и цифры',
+            },
+          })}
+        />
+        {errors.username && errors.username.message && (
+          <p>{errors.username.message.toString()}</p>
+        )}
+      </label>
+      <label>
+        Пароль:
+        <input
+          className={errors.password ? 'invalid' : ''}
+          type='password'
+          placeholder='Пароль'
+          {...register('password', {
+            required: {
+              value: true,
+              message: 'Пароль обязателен.',
+            },
+            pattern: {
+              value: /^[a-z0-9_\-*]+$/,
+              message:
+                'Только цифры, маленькие английские буквы и символы "_","-","*"',
+            },
+          })}
+        />
+        {errors.password && errors.password.message && (
+          <p>{errors.password.message.toString()}</p>
+        )}
+      </label>
+      <button type='submit'>Вход</button>
+    </form>
   );
 }
 
