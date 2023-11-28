781347efab13d2b3bd3d3a6b53410078aea13dd6
andaluk <andaluk@gmail.com>
2023-11-29 00:50:23 +0300

    Реализовал End-to-End тесты экрана входа.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,22 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
+import mongoose from 'mongoose'
+import { MONGO_URL } from './src/const'
 
 export default defineConfig({
+  // setupNodeEvents can be defined in either
+  // the e2e or component configuration
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on) {
+      on('task', {
+        async 'db:seed'() {
+          mongoose.connect(MONGO_URL).then((obj) => {
+            obj.connection.db.collection('login-query').drop()
+          })
+          return null
+        },
+        //...
+      })
     },
   },
-});
+})
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,39 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('http://localhost:3000')
+import { sIn, pMail, pName, pPass, sUp } from '../../src/const'
+describe('Тестирование экрана входа', () => {
+  const mail = 'test@test.com'
+  const name = 'testname'
+  const password = 'password'
+  before(() => {
+    cy.task('db:seed')
+  })
+  beforeEach(() => {
+    cy.visit('/')
+    cy.get('.menuBar').contains(sIn).click()
+  })
+  it('Вход несуществующим пользователем', () => {
+    cy.get(`input[placeholder=${pMail}]:visible`).type(mail)
+    cy.get(`input[placeholder=${pPass}]:visible`).type(password)
+    cy.get('button:visible').contains(sIn).click()
+    cy.get(':visible').should('contain', 'Ошибка авторизации')
+  })
+  it('Регистрация пользователя', () => {
+    cy.get('button:visible').contains(sUp).click()
+    cy.get(`input[placeholder=${pName}]:visible`).type(name)
+    cy.get(`input[placeholder=${pMail}]:visible`).first().type(mail)
+    cy.get(`input[placeholder=${pPass}]:visible`)
+      .first()
+      .type(password)
+      .next()
+      .type(password)
+    cy.get('button:visible').contains(sUp).click()
+    cy.get('.menuBar').trigger('mouseover').should('contain', mail)
+  })
+  it('Вход и выход пользователя', () => {
+    cy.get(`input[placeholder=${pMail}]:visible`).type(mail)
+    cy.get(`input[placeholder=${pPass}]:visible`).type(password)
+    cy.get('button:visible').contains(sIn).click()
+    cy.get('.menuBar').trigger('mouseover').should('contain', mail)
+    cy.get('.menuBar').trigger('mouseover').contains('Выйти').click()
+    cy.get('.menuBar').trigger('mouseover').should('not.contain', mail)
   })
 })
